d9da342b55076640c48da4141869ea85d0381605
korlkm <103992732+korlkm@users.noreply.github.com>
2023-04-03 23:41:51 +0900

    버그있음

--- app/src/main/java/com/example/letseatingtime/MainActivity.kt
+++ app/src/main/java/com/example/letseatingtime/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.letseatingtime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import coil.load
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
