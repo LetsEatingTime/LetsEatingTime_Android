c9552057c2600f97f64c8bb68a183dc5c6421603
korlkm <korlkm@dgsw.hs.kr>
2023-04-14 14:39:43 +0900

    home 제작

--- app/src/main/java/com/alt/letseatingtime_android/MainActivity.kt
+++ app/src/main/java/com/alt/letseatingtime_android/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.alt.letseatingtime_android
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.renderscript.ScriptGroup.Binding
 import android.util.Log
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.alt.letseatingtime_android.databinding.ActivityMainBinding
 import com.alt.letseatingtime_android.network.retrofit.RetrofitClient
 import com.alt.letseatingtime_android.network.retrofit.response.MealResponse
@@ -21,6 +28,9 @@ class MainActivity : AppCompatActivity() {
         Log.d("상태","onCreate()")
         mbinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initImageViewProfile()
+
         RetrofitClient.apiMeal.meal("2023","3","21").enqueue(object : Callback<MealResponse>{
             override fun onResponse(call: Call<MealResponse>, response: Response<MealResponse>) {
                 if(response.code() == 200){
@@ -58,4 +68,100 @@ class MainActivity : AppCompatActivity() {
         Log.d("상태","onDestroy()")
         mbinding = null
     }
+
+
+    private fun initImageViewProfile() {
+
+        binding.profileEdit.setOnClickListener {
+            when {
+                // 갤러리 접근 권한이 있는 경우
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+                -> {
+                    navigateGallery()
+                }
+
+                // 갤러리 접근 권한이 없는 경우 & 교육용 팝업을 보여줘야 하는 경우
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                -> {
+                    showPermissionContextPopup()
+                }
+
+                // 권한 요청 하기(requestPermissions) -> 갤러리 접근(onRequestPermissionResult)
+                else -> requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1000
+                )
+            }
+
+        }
+    }
+
+    // 권한 요청 승인 이후 실행되는 함수
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    navigateGallery()
+                else
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                //
+            }
+        }
+    }
+
+    private fun navigateGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        // 가져올 컨텐츠들 중에서 Image 만을 가져온다.
+        intent.type = "image/*"
+        // 갤러리에서 이미지를 선택한 후, 프로필 이미지뷰를 수정하기 위해 갤러리에서 수행한 값을 받아오는 startActivityForeResult를 사용한다.
+        startActivityForResult(intent, 2000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // 예외처리
+        if (resultCode != Activity.RESULT_OK)
+            return
+
+        when (requestCode) {
+            // 2000: 이미지 컨텐츠를 가져오는 액티비티를 수행한 후 실행되는 Activity 일 때만 수행하기 위해서
+            2000 -> {
+                val selectedImageUri: Uri? = data?.data
+                if (selectedImageUri != null) {
+                    binding.studentImage.setImageURI(selectedImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("프로필 이미지를 바꾸기 위해서는 갤러리 접근 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+    }
+
+
+
 }
