bf29f55f899f61a8e0b3d29ab17aa46cb443ad9a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-17 08:19:18 +0900

    :alien: change external api

--- app/src/main/java/com/alt/letseatingtime_android/network/retrofit/request/SignupRequest.kt
+++ app/src/main/java/com/alt/letseatingtime_android/network/retrofit/request/SignupRequest.kt
@@ -3,16 +3,18 @@ package com.alt.letseatingtime_android.network.retrofit.request
 import com.google.gson.annotations.SerializedName
 
 data class SignupRequest(
+    @SerializedName("id")
+    val id: String,
     @SerializedName("name")
     val name: String,
     @SerializedName("password")
     val password: String,
-    @SerializedName("schoolNumber")
-    val schoolNumber: String,
-    @SerializedName("lastTime")
-    val lastTime: String,
-    @SerializedName("mealApplication")
-    val mealApplication: String,
     @SerializedName("userType")
-    val userType: Char
+    val userType: Char,
+    @SerializedName("grade")
+    val grade: Int,
+    @SerializedName("className")
+    val className: Int,
+    @SerializedName("classNo")
+    val classNo: Int
 )
