e91ada64797e12d328d51daa9a4ee2d5e5e6da0b
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-17 08:17:02 +0900

    :alien: feed api change

--- app/src/main/java/com/alt/letseatingtime_android/ui/activity/MainActivity.kt
+++ app/src/main/java/com/alt/letseatingtime_android/ui/activity/MainActivity.kt
@@ -6,10 +6,9 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.alt.letseatingtime_android.network.retrofit.RetrofitClient
-import com.alt.letseatingtime_android.network.retrofit.response.MealResponse
+import com.alt.letseatingtime_android.network.retrofit.response.meal.MealResponse
 import com.example.letseatingtime.R
 import com.example.letseatingtime.databinding.ActivityMainBinding
 import retrofit2.Call
@@ -26,21 +25,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val current = LocalDateTime.now()
         val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-        RetrofitClient.apiMeal.meal(current.format(formatter))
+        RetrofitClient.api.meal(current.format(formatter))
             .enqueue(object : Callback<MealResponse> {
                 override fun onResponse(
                     call: Call<MealResponse>,
                     response: retrofit2.Response<MealResponse>
                 ) {
                     if (response.code() == 200) {
-                        Log.d("상태", "${response.body()}")
+                        Log.d("밥", "${response.body()}")
                     } else {
-                        Log.d("상태", "${response.code()}")
+                        Log.d("밥", "${response.code()}")
                     }
                 }
 
                 override fun onFailure(call: Call<MealResponse>, t: Throwable) {
-                    Log.d("상태", t.message.toString())
+                    Log.d("밥", t.message.toString())
                 }
             })
 
