d08cf1b58aca2ee80c73e1977bab81b3e4cc4870
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-17 08:18:56 +0900

    :alien: change external api

--- app/src/main/java/com/alt/letseatingtime_android/network/retrofit/response/MealResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.alt.letseatingtime_android.network.retrofit.response
-
-data class MealResponse(
-    val data: Data,
-    val message: String,
-    val status: Int
-)
---
+++ app/src/main/java/com/alt/letseatingtime_android/network/retrofit/response/meal/MealResponse.kt
@@ -0,0 +1,6 @@
+package com.alt.letseatingtime_android.network.retrofit.response.meal
+
+data class MealResponse(
+    val data: Data,
+    val status: Int
+)
