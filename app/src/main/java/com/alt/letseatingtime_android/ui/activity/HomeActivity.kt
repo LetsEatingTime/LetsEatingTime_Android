6805828203bebb7aaa64635ae8cad56ee92690e2
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-22 11:59:35 +0900

    :white_check_mark: Feeding api via http communication

--- app/src/main/java/com/alt/letseatingtime_android/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/alt/letseatingtime_android/ui/activity/HomeActivity.kt
@@ -7,12 +7,17 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.letseatingtime.R
 import com.example.letseatingtime.databinding.ActivityHomeBinding
 import com.example.letseatingtime.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class HomeActivity : AppCompatActivity() {
     private val binding: ActivityHomeBinding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
@@ -20,6 +25,34 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initImageViewProfile()
+
+
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+        RetrofitClient.api.meal(current.format(formatter))
+            .enqueue(object : Callback<MealResponse> {
+                override fun onResponse(
+                    call: Call<MealResponse>,
+                    response: retrofit2.Response<MealResponse>
+                ) {
+                    if (response.code() == 200) {
+                        Log.d("밥", "${response.body()}")
+                    } else {
+                        Log.d("밥", "${response.code()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<MealResponse>, t: Throwable) {
+                    Log.d("밥", t.message.toString())
+                }
+            })
+
+        //로그아웃
+        binding.logout.setOnClickListener {
+            val intent: Intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
     private fun initImageViewProfile() {
