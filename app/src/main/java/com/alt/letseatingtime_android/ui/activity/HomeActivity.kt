2a13364b314b6ef593bde8010f2d7657d3b26202
korlkm <103992732+korlkm@users.noreply.github.com>
2023-05-29 23:50:41 +0900

    :zap: 프로필 설정 (SDK 33)은 안됨

--- app/src/main/java/com/alt/letseatingtime_android/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/alt/letseatingtime_android/ui/activity/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.alt.letseatingtime_android.ui.activity
 
+import android.Manifest
 import android.content.Intent
-import android.os.Build
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import androidx.annotation.RequiresApi
+import android.widget.ImageView
 import com.alt.letseatingtime_android.MyApplication.Companion.prefs
 import com.alt.letseatingtime_android.network.retrofit.RetrofitClient
 import com.alt.letseatingtime_android.network.retrofit.response.meal.MealResponse
@@ -17,19 +19,35 @@ import java.time.format.DateTimeFormatter
 
 class HomeActivity : AppCompatActivity() {
     private val binding: ActivityHomeBinding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
-    @RequiresApi(Build.VERSION_CODES.O)
+
+
+
+    private val REQUEST_CODE = 1
+    private lateinit var profileImageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        profileImageView = binding.studentImage
+        val addPhotoButton = binding.profileEdit
+
+        addPhotoButton.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                requestPermissions(permissions, REQUEST_CODE)
+            } else {
+                openGallery()
+            }
+        }
 
         val stname = binding.someId
         val userName = prefs.userName // userName 값을 가져옵니다.
         stname.text = userName // TextView에 userName 값을 설정합니다.
 
         val textView = binding.trashId
-        val userClass = prefs.userGrade + "학년 " + prefs.userClassName + "반 " + prefs.userClassNo  + "번"
-
+        val userClass =
+            prefs.userGrade + "학년 " + prefs.userClassName + "반 " + prefs.userClassNo + "번"
         textView.text = userClass
 
 
@@ -58,6 +76,34 @@ class HomeActivity : AppCompatActivity() {
             val intent: Intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
+        }
+    }
+
+
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE && data != null) {
+            val imageUri = data.data
+            profileImageView.setImageURI(imageUri)
         }
     }
 
