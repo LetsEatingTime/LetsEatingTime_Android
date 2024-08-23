9a4563cd83ee26204ae23a1cac7581b81563d635
hom237 <jinsic4568@gmail.com>
2024-08-23 17:33:00 +0900

    :sparkles: create and implement store goods adapter for goods item recycler view

---
+++ app/src/main/java/com/alt/letseatingtime_android/ui/adapter/store/storedata/GoodsItem.kt
@@ -0,0 +1,7 @@
+package com.alt.letseatingtime_android.ui.adapter.store.storedata
+
+data class GoodsItem(
+    val name : String? = "베라 아이스크림",
+    val imageUrl : String ="",
+    val price : Int = 0
+)
