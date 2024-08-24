1cc0d7e717b2d6ea8e8c1911aee5222ee96c23bf
hom237 <jinsic4568@gmail.com>
2024-08-24 13:17:37 +0900

    :memo: add data class GoodsItem

--- app/src/main/java/com/alt/letseatingtime_android/ui/adapter/store/storedata/GoodsItem.kt
+++ app/src/main/java/com/alt/letseatingtime_android/ui/adapter/store/storedata/GoodsItem.kt
@@ -1,7 +1,8 @@
 package com.alt.letseatingtime_android.ui.adapter.store.storedata
 
 data class GoodsItem(
-    val name : String? = "베라 아이스크림",
-    val imageUrl : String ="",
-    val price : Int = 0
+    val id : Int,
+    val name : String? = "딸기라떼",
+    val imageUrl : String ="https://cdn.paris.spl.li/wp-content/uploads/2023/02/%EB%A6%AC%EC%96%BC%EB%94%B8%EA%B8%B0%EB%9D%BC%EB%96%BC_230323.jpg",
+    val price : Int = 5400
 )
