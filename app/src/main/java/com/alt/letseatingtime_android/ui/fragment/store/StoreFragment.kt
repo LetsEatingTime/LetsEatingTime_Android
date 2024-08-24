9c8cb495ca54be594ee7705de0aec88ff7d216c5
hom237 <jinsic4568@gmail.com>
2024-08-24 14:41:30 +0900

    :sparkles: making 1. input data in recommend goods recycler view 2. input data in all goods recycler view 3. move screen (StoreFragment page -> GoodsBuyFragment page)

--- app/src/main/java/com/alt/letseatingtime_android/ui/fragment/store/StoreFragment.kt
+++ app/src/main/java/com/alt/letseatingtime_android/ui/fragment/store/StoreFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.alt.letseatingtime.R
 import com.alt.letseatingtime.databinding.FragmentStoreBinding
+import com.alt.letseatingtime_android.ui.adapter.store.StoreGoods1Adapter
+import com.alt.letseatingtime_android.ui.adapter.store.StoreGoods2Adapter
+import com.alt.letseatingtime_android.ui.adapter.store.storedata.GoodsItem
+import com.alt.letseatingtime_android.ui.viewmodel.StoreViewModel
 
 
 class StoreFragment : Fragment() {
-    var _binding : FragmentStoreBinding? = null
+    var _binding: FragmentStoreBinding? = null
+    private val viewModel by activityViewModels<StoreViewModel>()
 
     private val binding get() = _binding!!
 
@@ -18,7 +26,26 @@ class StoreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentStoreBinding.inflate(inflater, container, false)
+        val testList = mutableListOf<GoodsItem>()
+        for (i in 0..10) {
+            testList.add(GoodsItem(i))
+        }
+        with(binding) {
+            rvForUserItems.adapter = StoreGoods1Adapter(testList) { position ->
+                moveScreen(testList[position])
+            }
+
+            rvUserItems.adapter = StoreGoods2Adapter(testList) { position ->
+                moveScreen(testList[position])
+            }
+        }
+
         return binding.root
+    }
+
+    private fun moveScreen(deliver: GoodsItem) {
+        viewModel.setGoodsData(deliver)
+        findNavController().navigate(R.id.action_storeFragment_to_goodsBuyFragment)
     }
 
 }
