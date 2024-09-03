9cef41a23892509d86a893d1af3e0668ec61ac44
hom237 <jinsic4568@gmail.com>
2024-09-03 20:47:36 +0900

    :lipstick: 사인업 이상한거 지우고, 비번변경 구현, 패턴 추가 후 경고 메세지

--- app/src/main/java/com/alt/letseatingtime_android/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/alt/letseatingtime_android/ui/activity/LoginActivity.kt
@@ -30,96 +30,102 @@ class LoginActivity : AppCompatActivity() {
      *
      * @param boolean up/down
      */
-    private fun loginBtnAnim(boolean: Boolean) {
-        val startColor =
-            ContextCompat.getColor(this, if (!boolean) R.color.button else R.color.button_push)
-        val endColor = ContextCompat.getColor(
-            this, if (!boolean) R.color.button else R.color.button_push
-        )
-        // ValueAnimator 생성
-        val animator = ValueAnimator.ofObject(ArgbEvaluator(), startColor, endColor)
-        animator.duration = 60 // 애니메이션 지속 시간 (밀리초)
-
-        animator.addUpdateListener {
-            val animatedValue = animator.animatedValue as Int
-            binding.btnLoginSubmit.background.setTint(animatedValue)
-        }
-
-        animator.start()
-    }
-
-    private val callback = object : OnBackPressedCallback(true) {
-        override fun handleOnBackPressed() {
-            // 뒤로 버튼 이벤트 처리
-            exit()
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        this.onBackPressedDispatcher.addCallback(this, callback)
-        binding.btnLoginSubmit.setOnClickListener(OnSingleClickListener {
-            val id = binding.etId.text.toString()
-            val pw = binding.etPw.text.toString()
-            if (id != "" && pw != "") {
-                loginBtnAnim(true)
-                binding.loginErrorMessage.text = ""
-                login(id = id, pw = pw)
-                binding.loginErrorMessage.visibility = View.GONE
-            } else {
-                binding.loginErrorMessage.visibility = View.VISIBLE
-                binding.loginErrorMessage.text = "비밀번호와 아이디를 입력해주세요"
-            }
-        })
-
-        binding.tvSignup.setOnClickListener(OnSingleClickListener {
-            Intent(this, SignupActivity::class.java).also {
-                startActivity(it)
-            }
-        })
-        binding.tvFindPw.setOnClickListener(OnSingleClickListener {
-            Toast.makeText(this, "아직 개발중인 기능입니다.", Toast.LENGTH_SHORT).show()
-        })
     }
+//    private fun loginBtnAnim(boolean: Boolean) {
+//        val startColor =
+//            ContextCompat.getColor(this, if (!boolean) R.color.button else R.color.button_push)
+//        val endColor = ContextCompat.getColor(
+//            this, if (!boolean) R.color.button else R.color.button_push
+//        )
+//        // ValueAnimator 생성
+//        val animator = ValueAnimator.ofObject(ArgbEvaluator(), startColor, endColor)
+//        animator.duration = 60 // 애니메이션 지속 시간 (밀리초)
+//
+//        animator.addUpdateListener {
+//            val animatedValue = animator.animatedValue as Int
+//            binding.btnLoginSubmit.background.setTint(animatedValue)
+//        }
+//
+//        animator.start()
+//    }
+//
+//    private val callback = object : OnBackPressedCallback(true) {
+//        override fun handleOnBackPressed() {
+//            // 뒤로 버튼 이벤트 처리
+//            exit()
+//        }
+//    }
 
-    private fun login(id: String, pw: String) {
-        RetrofitClient.api.login(LoginRequest(id = id, password = pw))
-            .enqueue(object : Callback<LoginResponse> {
-                override fun onResponse(
-                    call: Call<LoginResponse>,
-                    response: Response<LoginResponse>
-                ) {
-                    val result = response.body()
-                    if (response.isSuccessful) {
-                        binding.loginErrorMessage.visibility = View.GONE
-                        val intent = Intent(this@LoginActivity, HomeActivity::class.java)
-                        if (binding.cbLogin.isChecked) {
-                            prefs.autoLogin = true
-                            prefs.refreshToken = result?.data?.refreshToken.toString()
-                            prefs.accessToken = result?.data?.accessToken.toString()
-                        } else {
-                            prefs.autoLogin = false
-                            prefs.accessToken = result?.data?.accessToken.toString()
-                        }
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        binding.loginErrorMessage.visibility = View.VISIBLE
-                        binding.loginErrorMessage.text = "비밀번호나 아이디가 틀렸습니다"
-                        loginBtnAnim(false)
-                    }
-                }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(binding.root)
 
-                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                    Toast.makeText(this@LoginActivity, "서버 애러", Toast.LENGTH_SHORT).show()
-                    loginBtnAnim(false)
-                }
-            })
-    }
+//        this.onBackPressedDispatcher.addCallback(this, callback)
+//        binding.btnLoginSubmit.setOnClickListener(OnSingleClickListener {
+//            val id = binding.etId.text.toString()
+//            val pw = binding.etPw.text.toString()
+//            if (id != "" && pw != "") {
+//                loginBtnAnim(true)
+//                binding.loginErrorMessage.text = ""
+//                login(id = id, pw = pw)
+//                binding.loginErrorMessage.visibility = View.GONE
+//            } else {
+//                binding.loginErrorMessage.visibility = View.VISIBLE
+//                binding.loginErrorMessage.text = "비밀번호와 아이디를 입력해주세요"
+//            }
+//        })
+//
+//        binding.tvSignup.setOnClickListener(OnSingleClickListener {
+//            Intent(this, SignupActivity::class.java).also {
+//                startActivity(it)
+//            }
+//        })
+//        binding.tvFindPw.setOnClickListener(OnSingleClickListener {
+//            Toast.makeText(this, "아직 개발중인 기능입니다.", Toast.LENGTH_SHORT).show()
+//        })
+//    }
 
-    fun exit() {
-        ActivityCompat.finishAffinity(this)
-    }
+//    private fun login(id: String, pw: String) {
+//        RetrofitClient.api.login(LoginRequest(id = id, password = pw))
+//            .enqueue(object : Callback<LoginResponse> {
+//                override fun onResponse(
+//                    call: Call<LoginResponse>,
+//                    response: Response<LoginResponse>
+//                ) {
+//                    val result = response.body()
+//                    if (response.isSuccessful) {
+//                        binding.loginErrorMessage.visibility = View.GONE
+//                        val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+//                        if (binding.cbLogin.isChecked) {
+//                            prefs.autoLogin = true
+//                            prefs.refreshToken = result?.data?.refreshToken.toString()
+//                            prefs.accessToken = result?.data?.accessToken.toString()
+//                        } else {
+//                            prefs.autoLogin = false
+//                            prefs.accessToken = result?.data?.accessToken.toString()
+//                        }
+//                        startActivity(intent)
+//                        finish()
+//                    } else {
+//                        binding.loginErrorMessage.visibility = View.VISIBLE
+//                        binding.loginErrorMessage.text = "비밀번호나 아이디가 틀렸습니다"
+//                        loginBtnAnim(false)
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+//                    Toast.makeText(this@LoginActivity, "서버 애러", Toast.LENGTH_SHORT).show()
+//                    loginBtnAnim(false)
+//                }
+//            })
+//    }
+
+//    fun exit() {
+//        ActivityCompat.finishAffinity(this)
+//    }
 }
