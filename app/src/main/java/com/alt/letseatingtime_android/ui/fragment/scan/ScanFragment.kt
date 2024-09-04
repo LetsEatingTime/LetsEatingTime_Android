51900afaede385dbbe86f64f2e0ff08c7a38f972
hom237 <jinsic4568@gmail.com>
2024-09-05 00:31:28 +0900

    :sparkles: define move screen

--- app/src/main/java/com/alt/letseatingtime_android/ui/fragment/scan/ScanFragment.kt
+++ app/src/main/java/com/alt/letseatingtime_android/ui/fragment/scan/ScanFragment.kt
@@ -5,18 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.alt.letseatingtime.R
 import com.alt.letseatingtime.databinding.FragmentScanBinding
 
 
 class ScanFragment : Fragment() {
-    private var _binding : FragmentScanBinding? = null
-    private val binding get()=_binding!!
+    private var _binding: FragmentScanBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentScanBinding.inflate(inflater, container, false)
+
+        binding.ibBackButton.setOnClickListener {
+            findNavController().navigate(R.id.action_scanFragment_to_homeFragment2)
+        }
         return binding.root
     }
 }
